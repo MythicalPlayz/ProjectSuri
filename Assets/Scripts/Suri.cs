0d34b0bae3d7acfe03e57de850498869020357af
MythicalPlayz <eyaddodo05@gmail.com>
2026-04-22 16:25:42 +0200

    suri1

---
+++ Assets/Scripts/Suri.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Suri : MonoBehaviour
+{
+    public int mainType; // 0 = none, 1 = fries, 2 = chicken, 3 = both
+    public int cookType; // 0 = undercooked, 1 = cooked, 2 = overcooked
+    public bool ketchup; // 0 = none, 1 = have;
+    public bool mustaard;
+    public bool mayo;
+    public bool garlic;
+    public bool tomatos;
+    public bool cheese;
+    public bool spicy;
+    public bool pepper;
+    public bool wrapper;
+    public bool grillMarks;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
